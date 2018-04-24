d38f65c72b69dc5b281278fbe219a0a1ce38bc5c
John Lombardo <john@lombardos.org>
2018-04-24 11:36:30 -0700

    Add git update-index command to doc

--- azuredata/src/androidTest/java/com/azure/data/integration/DoNotDistribute.kt
+++ azuredata/src/androidTest/java/com/azure/data/integration/DoNotDistribute.kt
@@ -1,8 +1,12 @@
 package com.azure.data.integration
 
 /*
-This file contains keys and database ids that should not be pushed to the git repository
-The file is included in .gitignore, so any changes to it will not be saved to the repository.
+This file contains keys and database ids that MUST not be pushed to the git repository
+Once you've downloaded the repository to your local machine, execute  following git
+command to ensure that any changes to this file will be ignored by git:
+
+git update-index --assume-unchanged azuredata/src/androidTest/java/com/azure/data/integration/DoNotDistribute.kt
+
  */
 
 val azureCosmosDbAccount  = "" // Usually matches "database2<12MoreLowerCaseCharacters>"
