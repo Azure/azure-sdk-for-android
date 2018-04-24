1e8f579f74f23ac9078542c6dd72e3b91471b64e
John Lombardo <john@lombardos.org>
2018-04-24 11:20:00 -0700

    Move keys to a file that can be ignored

---
+++ azuredata/src/androidTest/java/com/azure/data/integration/DoNotDistribute.kt
@@ -0,0 +1,9 @@
+package com.azure.data.integration
+
+/*
+This file contains keys and database ids that should not be pushed to the git repository
+The file is included in .gitignore, so any changes to it will not be saved to the repository.
+ */
+
+val azureCosmosDbAccount  = "" // Usually matches "database2<12MoreLowerCaseCharacters>"
+val azureCosmosPrimaryKey = "" // Matches "<86CharsOfUpperAndLowerCaseWithNumbers>=="
