fa239774ef3017ca6188c7bac101ce4cf651888e
John Lombardo <john@lombardos.org>
2018-04-25 11:10:24 -0700

    Write failing test

--- azuredata/src/androidTest/java/com/azure/data/integration/UserTests.kt
+++ azuredata/src/androidTest/java/com/azure/data/integration/UserTests.kt
@@ -7,7 +7,9 @@ import com.azure.data.model.Database
 import com.azure.data.model.ResourceType
 import com.azure.data.model.User
 import com.azure.data.service.DataResponse
+import com.azure.data.service.ListResponse
 import com.azure.data.service.Response
+import junit.framework.Assert
 import junit.framework.Assert.assertEquals
 import org.awaitility.Awaitility.await
 import org.junit.After
@@ -51,6 +53,20 @@ class UserTests : ResourceTest<User>(ResourceType.User, true, false) {
         await().until {
             deleteResponse != null
         }
+    }
+
+    private fun createNewUsers(count: Int, db: Database? = null) : List<User> {
+        val users = mutableListOf<User>()
+        for(i in 1..count){
+            val userId = "$createdResourceId${if (i==1) "" else i.toString()}"
+            AzureData.createUser(userId, databaseId){
+                assertResourceResponseSuccess(it)
+                assertEquals(userId, it.resource?.id)
+                users.add(it.resource!!)
+            }
+        }
+        await().until { users.count() == count }
+        return users
     }
 
     private fun createNewUser(db: Database? = null) : User {
@@ -105,6 +121,67 @@ class UserTests : ResourceTest<User>(ResourceType.User, true, false) {
         }
 
         assertListResponseSuccess(resourceListResponse)
+    }
+
+    @Test
+    fun listUsersPaging() {
+
+        val idsFound = mutableListOf<String>()
+        var waitForResponse : ListResponse<User>? = null
+
+        createNewUsers(3)
+
+        // Get the first one
+        AzureData.getUsers(databaseId, 1) { waitForResponse = it }
+        await().until { waitForResponse != null }
+        waitForResponse.let {
+            Assert.assertNotNull(it!!.metadata.continuation)
+            Assert.assertNotNull(it.resource?.items)
+            assertEquals(1,it.resource?.items?.size)
+            val id = it.resource?.items?.get(0)?.id!!
+            Assert.assertTrue(id.startsWith(createdResourceId))
+            Assert.assertFalse(idsFound.contains(id))
+            idsFound.add(id)
+            Assert.assertTrue(it.hasMoreResults)
+        }
+
+        // Get the second one
+        waitForResponse.let { response ->
+            waitForResponse = null
+            response!!.next<User> {
+                Assert.assertNotNull(it.metadata.continuation)
+                Assert.assertNotNull(it.resource?.items)
+                assertEquals(1,it.resource?.items?.size)
+                val id = it.resource?.items?.get(0)?.id!!
+                Assert.assertTrue(id.startsWith(createdResourceId))
+                Assert.assertFalse(idsFound.contains(id))
+                idsFound.add(id)
+                Assert.assertTrue(it.hasMoreResults)
+                waitForResponse = it
+            }
+        }
+        await().until { waitForResponse != null }
+
+        // Get the third one
+        waitForResponse.let { response ->
+            waitForResponse = null
+            response!!.next<User> {
+                Assert.assertNotNull(it.resource?.items)
+                assertEquals(1,it.resource?.items?.size)
+                val id = it.resource?.items?.get(0)?.id!!
+                Assert.assertTrue(id.startsWith(createdResourceId))
+                Assert.assertFalse(idsFound.contains(id))
+                idsFound.add(id)
+                Assert.assertFalse(it.hasMoreResults)
+                waitForResponse = it
+            }
+        }
+        await().until { waitForResponse != null }
+
+        // Try to get one more
+        waitForResponse!!.next<User> {
+            assertErrorResponse(it)
+        }
     }
 
     @Test
