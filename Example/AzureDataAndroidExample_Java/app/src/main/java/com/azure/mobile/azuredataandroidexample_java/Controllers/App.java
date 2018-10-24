ff176f0ccb488996743c6a39a6229106cecd2600
Nate Rickard <NateRickard@gmail.com>
2018-10-24 18:44:14 -0400

    fixed configure issue from Java

--- Example/AzureDataAndroidExample_Java/app/src/main/java/com/azure/mobile/azuredataandroidexample_java/Controllers/App.java
+++ Example/AzureDataAndroidExample_Java/app/src/main/java/com/azure/mobile/azuredataandroidexample_java/Controllers/App.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.azure.data.AzureData;
 import com.azure.data.model.PermissionMode;
 
+import static com.azure.data.util.FunctionalUtils.onCallback;
+
 public class App extends Application {
 
     @Override
@@ -12,6 +14,8 @@ public class App extends Application {
         super.onCreate();
 
         // configure AzureData - fill in your account name and master key
-        AzureData.configure(getApplicationContext(), "", "", PermissionMode.All);
+        AzureData.configure(getApplicationContext(), "", "", PermissionMode.All, onCallback(builder -> {
+
+        }));
     }
 }
