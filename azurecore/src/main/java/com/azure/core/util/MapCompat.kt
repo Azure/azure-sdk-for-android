0c629d1b559a89b349756b6738610c1664b745b9
Nate Rickard <NateRickard@gmail.com>
2019-01-28 12:13:36 -0500

    updated gradle plugin & kotlin versions

---
+++ azurecore/src/main/java/com/azure/core/util/MapCompat.kt
@@ -0,0 +1,18 @@
+package com.azure.core.util
+
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+//class MapCompat {
+//
+//    fun <K, V> getOrDefault(@NonNull map: Map<K, V>, key: K, defaultValue: V): V? {
+//
+//        val v: V
+//
+//        return if ((v = map[key]) != null || map.containsKey(key))
+//            v
+//        else
+//            defaultValue
+//    }
+//}
