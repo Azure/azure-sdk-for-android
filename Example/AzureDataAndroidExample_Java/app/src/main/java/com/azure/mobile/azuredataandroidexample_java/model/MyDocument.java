12d9b5e3e69e01f09683b23d0b84a232c836a0d5
Nate Rickard <NateRickard@gmail.com>
2019-02-20 16:46:43 -0500

    updated Java sample to use customer document type

---
+++ Example/AzureDataAndroidExample_Java/app/src/main/java/com/azure/mobile/azuredataandroidexample_java/model/MyDocument.java
@@ -0,0 +1,19 @@
+package com.azure.mobile.azuredataandroidexample_java.model;
+
+import com.azure.data.model.Document;
+import com.azure.data.model.ResourceLocation;
+import java.util.Date;
+
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+public class MyDocument extends Document {
+
+    String testString = "My Custom String";
+    Integer testNumber = 0;
+    Date testDate = new Date();
+    Boolean testBool = false;
+    int[] testArray = {1, 2, 3};
+    ResourceLocation.User testObject = null;
+}
