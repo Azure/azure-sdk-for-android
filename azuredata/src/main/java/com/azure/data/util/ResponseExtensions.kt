657e6ce001ca6e6cd9282ab2a7d7e9c68df586fc
Nate Rickard <NateRickard@gmail.com>
2019-04-09 14:40:58 -0400

    used enum value

--- azuredata/src/main/java/com/azure/data/util/ResponseExtensions.kt
+++ azuredata/src/main/java/com/azure/data/util/ResponseExtensions.kt
@@ -1,7 +1,8 @@
 package com.azure.data.util
 
+import com.azure.core.http.HttpStatusCode
 import com.azure.data.model.service.Response
 
 fun <T> Response<T>.is404(): Boolean {
-    return this.response?.code() == 404
+    return this.response?.code() == HttpStatusCode.NotFound.code
 }
