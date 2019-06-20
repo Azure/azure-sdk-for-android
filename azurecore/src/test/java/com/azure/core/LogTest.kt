a48ced1670dcd0ae2a31801cefc45f2f2d863529
Nate Rickard <NateRickard@gmail.com>
2019-06-20 16:57:05 -0400

    fixed warning(s)

--- azurecore/src/test/java/com/azure/core/LogTest.kt
+++ azurecore/src/test/java/com/azure/core/LogTest.kt
@@ -12,26 +12,32 @@ import timber.log.Timber
 class LogTest {
 
     enum class Lev {
+
         ZERO_IS_NOT_A_LEVEL,
         ONE_IS_NOT_A_LEVEL,
-        verbose,
-        debug,
-        info,
-        warn,
-        error,
-        assert,
+        Verbose,
+        Debug,
+        Info,
+        Warn,
+        Error,
+        Assert,
     }
 
     class ArrayTree : Timber.Tree() {
+
         data class Entry( val priority: Int, val tag: String?, val message: String, val throwable: Throwable?)
         val log = ArrayList<Entry>()
+
         override fun log(priority: Int, tag: String?, message: String, throwable: Throwable?) {
+
             log.add(Entry(priority,tag,message,throwable))
         }
+
         fun clear() : ArrayTree {
             log.clear()
             return this
         }
+
         fun assert(index: Int, priority: Int, tag: String?, message: String, throwable: Throwable?) {
             assertEquals(true,index<log.size)
             assertEquals(priority, log[index].priority)
@@ -45,44 +51,48 @@ class LogTest {
 
     @Before
     fun before() {
+
         startLogging(tree)
     }
 
     @After
     fun after() {
+
         stopLogging(tree)
     }
 
     @Test @Throws(Exception::class) fun can_log_each_level() {
+
         var index = 0
         tree.clear()
         assertEquals(0, tree.log.size)
-        v{Lev.verbose.name}
-        tree.assert(index++, Log.VERBOSE,null, Lev.verbose.name,null)
-        d{Lev.debug.name}
-        tree.assert(index++, Log.DEBUG,null, Lev.debug.name,null)
-        i{Lev.info.name}
-        tree.assert(index++, Log.INFO,null, Lev.info.name,null)
-        w{Lev.warn.name}
-        tree.assert(index++,Log.WARN,null,Lev.warn.name,null)
-        e{Lev.error.name}
-        tree.assert(index,Log.ERROR,null,Lev.error.name,null)
+        v{Lev.Verbose.name}
+        tree.assert(index++, Log.VERBOSE,null, Lev.Verbose.name,null)
+        d{Lev.Debug.name}
+        tree.assert(index++, Log.DEBUG,null, Lev.Debug.name,null)
+        i{Lev.Info.name}
+        tree.assert(index++, Log.INFO,null, Lev.Info.name,null)
+        w{Lev.Warn.name}
+        tree.assert(index++,Log.WARN,null,Lev.Warn.name,null)
+        e{Lev.Error.name}
+        tree.assert(index,Log.ERROR,null,Lev.Error.name,null)
     }
 
     @Test @Throws(Exception::class) fun can_change_levels() {
+
         var index = 0
         tree.clear()
         startLogging(Log.WARN)
         assertEquals(0, tree.log.size)
-        v{Lev.verbose.name}
+        v{Lev.Verbose.name}
         assertEquals(0, tree.log.size)
-        d{Lev.debug.name}
+        d{Lev.Debug.name}
         assertEquals(0, tree.log.size)
-        i{Lev.info.name}
+        i{Lev.Info.name}
         assertEquals(0, tree.log.size)
-        w{Lev.warn.name}
-        tree.assert(index++,Log.WARN,null,Lev.warn.name,null)
-        e{Lev.error.name}
-        tree.assert(index++,Log.ERROR,null,Lev.error.name,null)
+        w{Lev.Warn.name}
+        tree.assert(index++,Log.WARN,null,Lev.Warn.name,null)
+        e{Lev.Error.name}
+        tree.assert(index,Log.ERROR,null,Lev.Error.name,null)
     }
 }
