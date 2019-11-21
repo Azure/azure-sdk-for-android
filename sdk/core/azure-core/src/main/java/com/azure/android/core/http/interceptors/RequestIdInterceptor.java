c063b1212e939dc27853ac75c9e9f1c42f275eac
Victor Colin Amador <vicolina@microsoft.com>
2019-11-20 22:25:49 -0800

    Created RequestIdInterceptor

---
+++ sdk/core/azure-core/src/main/java/com/azure/android/core/http/interceptors/RequestIdInterceptor.java
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.core.http.interceptors;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * The pipeline interceptor that puts a UUID in the request header. Azure uses the request ID as
+ * the unique identifier for the request.
+ */
+public class RequestIdInterceptor implements Interceptor {
+    private static final String REQUEST_ID_HEADER = "x-ms-client-request-id";
+
+    @NotNull
+    @Override
+    public Response intercept(@NotNull Chain chain) throws IOException {
+        Request request = chain.request();
+        String requestId = request.header(REQUEST_ID_HEADER);
+
+        if (requestId == null)
+            request = request.newBuilder()
+                .addHeader(REQUEST_ID_HEADER, UUID.randomUUID().toString())
+                .build();
+
+        return chain.proceed(request);
+    }
+}
