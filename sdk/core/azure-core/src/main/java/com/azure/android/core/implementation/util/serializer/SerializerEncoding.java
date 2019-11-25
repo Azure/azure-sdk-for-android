93bba4eb1b4ff669841f27a674a713f3903b6865
Anu Thomas Chandy <anuamd@hotmail.com>
2019-11-25 12:45:54 -0800

    Initial version of Retrofit API Client

---
+++ sdk/core/azure-core/src/main/java/com/azure/android/core/implementation/util/serializer/SerializerEncoding.java
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.core.implementation.util.serializer;
+
+/**
+ * Supported serialization encoding formats.
+ */
+public enum SerializerEncoding {
+    /**
+     * JavaScript Object Notation.
+     */
+    JSON,
+
+    /**
+     * Extensible Markup Language.
+     */
+    XML;
+}
