92dc7e754c1aa0c834430784c69057ec45cc4bef
Anu Thomas Chandy <anuamd@hotmail.com>
2019-11-26 12:27:25 -0800

    Exception util create Exception based on response code

---
+++ sdk/core/azure-core/src/main/java/com/azure/android/core/implementation/util/ExceptionUtils.java
@@ -0,0 +1,91 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.core.implementation.util;
+
+import com.azure.android.core.exception.HttpResponseException;
+import com.azure.android.core.implementation.util.serializer.SerializerAdapter;
+import com.azure.android.core.implementation.util.serializer.SerializerEncoding;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+/**
+ * Internal utility type to deal with exception.
+ */
+public class ExceptionUtils {
+    private ExceptionUtils() {
+    }
+
+    /**
+     * Create an exception from the http response.
+     *
+     * @param exceptionMapping the http status code to exception mapping
+     * @param response the failed response from the service
+     * @param serializerAdapter the adapter to decode error body
+     * @return the exception
+     */
+    public static RuntimeException createException(Map<Integer, Class<? extends HttpResponseException>> exceptionMapping,
+                                                   Response response,
+                                                   SerializerAdapter serializerAdapter) {
+        Class<? extends HttpResponseException> exceptionType = null;
+        Class<? extends HttpResponseException> defaultExceptionType = null;
+        for (Map.Entry<Integer, Class<? extends HttpResponseException>> mapping : exceptionMapping.entrySet()) {
+            if (mapping.getKey() == response.code()) {
+                exceptionType = mapping.getValue();
+            } else if (mapping.getKey() == -1) {
+                defaultExceptionType = mapping.getValue();
+            }
+        }
+        exceptionType = exceptionType == null
+                ? (defaultExceptionType == null ? HttpResponseException.class : defaultExceptionType)
+                : exceptionType;
+
+        //
+        Class<?> exceptionValueType = Object.class;
+        String errorContent = "";
+        Object errorContentDecoded = null;
+        final ResponseBody errorBody = response.body();
+        if (errorBody != null && errorBody.source() != null) {
+            errorContent = errorBody.source().getBuffer().readUtf8();
+            if (errorContent.length() >= 0) {
+                try {
+                    final Method exceptionValueMethod = exceptionType.getDeclaredMethod("value");
+                    exceptionValueType = exceptionValueMethod.getReturnType();
+                } catch (NoSuchMethodException e) {
+                    exceptionValueType = Object.class;
+                }
+                try {
+                    errorContentDecoded = serializerAdapter.deserialize(errorContent,
+                            exceptionValueType,
+                            SerializerEncoding.fromHeaders(response.headers()));
+                } catch (IOException ignored) {
+                    // ignored
+                }
+            }
+        }
+        String errorBodyRepresentation = errorContent.isEmpty() ? "(empty body)" : "\"" + errorContent + "\"";
+        //
+        RuntimeException exception;
+        try {
+            final Constructor<? extends HttpResponseException> exceptionConstructor = exceptionType.getConstructor(String.class,
+                    Response.class,
+                    exceptionValueType);
+            exception = exceptionConstructor.newInstance("Status code " + response.code() + ", " + errorBodyRepresentation,
+                    response,
+                    errorContentDecoded);
+        } catch (ReflectiveOperationException e) {
+            String message = "Status code " + response.code() + ", but an instance of "
+                    + exceptionType.getCanonicalName() + " cannot be created."
+                    + " Response body: " + errorBodyRepresentation;
+            exception = new RuntimeException(new IOException(message, e));
+        }
+        return exception;
+    }
+
+}
---
+++ sdk/core/azure-core/src/main/java/com/azure/android/core/implementation/util/serializer/SerializerAdapter.java
@@ -0,0 +1,56 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.core.implementation.util.serializer;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import okhttp3.Headers;
+
+/**
+ * An interface defining the behaviors of a serializer.
+ */
+public interface SerializerAdapter {
+    /**
+     * Serializes an object into a string.
+     *
+     * @param object the object to serialize
+     * @param encoding the encoding to use for serialization
+     * @return the serialized string. Null if the object to serialize is null
+     * @throws IOException exception from serialization
+     */
+    String serialize(Object object, SerializerEncoding encoding) throws IOException;
+
+    /**
+     * Deserializes a string into a {@code U} object.
+     *
+     * @param value the string value to deserialize
+     * @param <U> the type of the deserialized object
+     * @param type the type to deserialize
+     * @param encoding the encoding used in the serialized value
+     * @return the deserialized object
+     * @throws IOException exception from deserialization
+     */
+    <U> U deserialize(String value, Type type, SerializerEncoding encoding) throws IOException;
+
+    /**
+     * Deserialize the provided headers returned from a REST API to an entity instance declared as
+     * the model to hold 'Matching' headers.
+     *
+     * @param headers the REST API returned headers
+     * @param <U> the type of the deserialized object
+     * @param type the type to deserialize
+     * @return instance of header entity type created based on provided {@code headers}, if header
+     * entity model does not exists then return null
+     * @throws IOException If an I/O error occurs
+     */
+    <U> U deserialize(Headers headers, Type type) throws IOException;
+
+    /**
+     * @return create and return default serializer;
+     */
+    static SerializerAdapter createDefault() {
+        return null;
+    }
+}
