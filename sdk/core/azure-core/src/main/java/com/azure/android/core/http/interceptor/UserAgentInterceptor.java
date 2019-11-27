7277d1666cb9162587ffeb40be2234e4916c2170
Victor Colin Amador <vicolina@microsoft.com>
2019-11-27 13:45:20 -0800

    Renamed the "interceptors" package to "interceptor".

--- sdk/core/azure-core/src/main/java/com/azure/android/core/http/interceptor/UserAgentInterceptor.java
+++ sdk/core/azure-core/src/main/java/com/azure/android/core/http/interceptor/UserAgentInterceptor.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.android.core.http.interceptors;
+package com.azure.android.core.http.interceptor;
 
 import androidx.annotation.NonNull;
 
