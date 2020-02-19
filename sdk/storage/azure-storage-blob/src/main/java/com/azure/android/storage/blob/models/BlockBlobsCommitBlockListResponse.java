b3bb01c46730de07cce2ccdb701b580f2b022f4e
Anu Thomas Chandy <anuamd@hotmail.com>
2020-02-18 16:14:39 -0800

    Storage: Adding models for blob stage and commit apis.

---
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/android/storage/blob/models/BlockBlobsCommitBlockListResponse.java
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.storage.blob.models;
+
+import com.azure.android.core.http.ResponseBase;
+
+/**
+ * Contains all response data for the commitBlockList operation.
+ */
+public final class BlockBlobsCommitBlockListResponse extends ResponseBase<BlockBlobCommitBlockListHeaders, Void> {
+    private final BlockBlobItem item;
+
+    /**
+     * Creates an instance of BlockBlobsCommitBlockListResponse.
+     *
+     * @param request the request which resulted in this BlockBlobsCommitBlockListResponse.
+     * @param statusCode the status code of the HTTP response.
+     * @param rawHeaders the raw headers of the HTTP response.
+     * @param value the deserialized value of the HTTP response.
+     * @param headers the deserialized headers of the HTTP response.
+     */
+    public BlockBlobsCommitBlockListResponse(okhttp3.Request request,
+                                             int statusCode,
+                                             okhttp3.Headers rawHeaders,
+                                             Void value,
+                                             BlockBlobCommitBlockListHeaders headers) {
+        super(request, statusCode, rawHeaders, value, headers);
+
+        this.item = new BlockBlobItem(headers.getETag(), headers.getLastModified(), headers.getContentMD5(),
+                headers.isServerEncrypted(), headers.getEncryptionKeySha256());
+    }
+
+    public BlockBlobItem getBlockBlobItem() {
+        return this.item;
+    }
+}
+
