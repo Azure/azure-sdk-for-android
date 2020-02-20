d9f7a0e3901099958315f5248ddd58e598a6eae5
Victor Colin Amador <vicolina@microsoft.com>
2020-02-19 18:13:40 -0800

    Modified the sample app to use a callback to handle blob downloads instead of an AsyncTask.

--- samples/sample-app-storage/src/main/java/com/azure/android/storage/sample/BlobItemViewHolder.java
+++ samples/sample-app-storage/src/main/java/com/azure/android/storage/sample/BlobItemViewHolder.java
@@ -1,17 +1,14 @@
 package com.azure.android.storage.sample;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,16 +16,15 @@ import androidx.annotation.NonNull;
 import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.azure.android.core.http.Callback;
 import com.azure.android.storage.blob.StorageBlobClient;
+import com.azure.android.storage.blob.models.BlobDownloadAsyncResponse;
 import com.azure.android.storage.blob.models.BlobItem;
 import com.azure.android.storage.sample.config.StorageConfiguration;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.ref.WeakReference;
-
-import okhttp3.ResponseBody;
 
 public class BlobItemViewHolder extends RecyclerView.ViewHolder {
     private final TextView blobName;
@@ -41,18 +37,49 @@ public class BlobItemViewHolder extends RecyclerView.ViewHolder {
     public static BlobItemViewHolder create(ViewGroup parent, StorageBlobClient storageBlobClient) {
         StorageConfiguration storageConfiguration = StorageConfiguration.create(parent.getContext());
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.blob_item, parent, false);
-        view.setOnClickListener(new View.OnClickListener() {
+        View blobItemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.blob_item, parent, false);
+        blobItemView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String blobName = (String) ((TextView) v.findViewById(R.id.blob_name)).getText();
-                View mainActivity = (View) parent.getParent().getParent().getParent();
+            public void onClick(View view) {
+                String blobName = (String) ((TextView) view.findViewById(R.id.blob_name)).getText();
+                String containerName = storageConfiguration.getContainerName();
 
-                new MyTask(v, mainActivity).execute(storageBlobClient, storageConfiguration.getContainerName(), blobName);
+                Toast.makeText(view.getContext(), "Downloading " + blobName, Toast.LENGTH_SHORT).show();
+
+                View mainActivityView = (View) parent.getParent().getParent().getParent();
+                showProgressBar(view, mainActivityView);
+
+                storageBlobClient.download(containerName, blobName, new Callback<BlobDownloadAsyncResponse>() {
+                        @Override
+                        public void onResponse(BlobDownloadAsyncResponse response) {
+                            File path = Environment.getExternalStorageDirectory();
+                            File file = new File(path, blobName);
+
+                            try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+                                fileOutputStream.write(response.getValue().bytes());
+                            } catch (IOException e) {
+                                Log.e("BlobViewItemHolder", "Error when downloading blob: " + containerName + "/" + blobName, e);
+                                onFailure(e);
+                            }
+
+                            hideProgressBar(view, mainActivityView);
+
+                            Toast.makeText(blobItemView.getContext(), "Download complete", Toast.LENGTH_SHORT).show();
+
+                            showFileIntent(view, file);
+                        }
+
+                        @Override
+                        public void onFailure(Throwable t) {
+                            hideProgressBar(view, mainActivityView);
+
+                            Toast.makeText(blobItemView.getContext(), "Download failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
             }
         });
 
-        return new BlobItemViewHolder(view);
+        return new BlobItemViewHolder(blobItemView);
     }
 
     public void bind(BlobItem blobItem) {
@@ -61,65 +88,28 @@ public class BlobItemViewHolder extends RecyclerView.ViewHolder {
         }
     }
 
-    private static final class MyTask extends AsyncTask<Object, Void, File> {
-        private final WeakReference<View> viewWeakReference;
-        private final WeakReference<Context> contextWeakReference;
-        private final WeakReference<View> backgroundWeakReference;
-        private final WeakReference<ProgressBar> progressBarWeakReference;
+    private static void showProgressBar(View view, View mainActivityView) {
+        mainActivityView.findViewById(R.id.download_progress_bar).setVisibility(View.VISIBLE);
+        mainActivityView.findViewById(R.id.download_background).setVisibility(View.VISIBLE);
+        ((Activity) view.getContext()).getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+    }
 
-        MyTask(View view, View mainActivity) {
-            viewWeakReference = new WeakReference<>(view);
-            contextWeakReference = new WeakReference<>(view.getContext());
-            backgroundWeakReference = new WeakReference<>(mainActivity.findViewById(R.id.download_background));
-            progressBarWeakReference = new WeakReference<>(mainActivity.findViewById(R.id.download_progress_bar));
-        }
+    private static void hideProgressBar(View view, View mainActivityView) {
+        ((Activity) view.getContext()).getWindow().clearFlags(
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        mainActivityView.findViewById(R.id.download_progress_bar).setVisibility(View.GONE);
+        mainActivityView.findViewById(R.id.download_background).setVisibility(View.GONE);
+    }
 
-        @Override
-        protected void onPreExecute() {
-            View view = viewWeakReference.get();
-            String blobName = (String) ((TextView) view.findViewById(R.id.blob_name)).getText();
-            Toast.makeText(contextWeakReference.get(), "Downloading " + blobName, Toast.LENGTH_SHORT).show();
-            backgroundWeakReference.get().setVisibility(View.VISIBLE);
-            progressBarWeakReference.get().setVisibility(View.VISIBLE);
-            ((Activity) contextWeakReference.get()).getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-        }
-
-        @Override
-        protected File doInBackground(Object... objects) {
-            StorageBlobClient storageBlobClient = (StorageBlobClient) objects[0];
-            String containerName = (String) objects[1];
-            String blobName = (String) objects[2];
-
-            File path = Environment.getExternalStorageDirectory();
-            File file = new File(path, blobName);
-
-            ResponseBody response = storageBlobClient.download(containerName, blobName);
-
-            try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
-                fileOutputStream.write(response.bytes());
-            } catch (IOException e) {
-                Log.e("BlobViewItemHolder", "Error when downloading blob: " + containerName + "/" + blobName, e);
-            }
-
-            return file;
-        }
-
-        @Override
-        protected void onPostExecute(File file) {
-            ((Activity) contextWeakReference.get()).getWindow().clearFlags(
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-            progressBarWeakReference.get().setVisibility(View.GONE);
-            backgroundWeakReference.get().setVisibility(View.GONE);
-            Toast.makeText(contextWeakReference.get(), "Download complete", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            Uri fileUri = FileProvider.getUriForFile(contextWeakReference.get(),
-                contextWeakReference.get().getApplicationContext().getPackageName() + ".provider",
-                file);
-            String contentType = contextWeakReference.get().getContentResolver().getType(fileUri);
-            intent.setDataAndType(fileUri, contentType);
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            contextWeakReference.get().startActivity(intent);
-        }
+    private static void showFileIntent(View view, File file) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri fileUri = FileProvider.getUriForFile(view.getContext(),
+            view.getContext().getApplicationContext().getPackageName() + ".provider",
+            file);
+        String contentType = view.getContext().getContentResolver().getType(fileUri);
+        intent.setDataAndType(fileUri, contentType);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        view.getContext().startActivity(intent);
     }
 }
