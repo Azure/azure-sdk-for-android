1f88936965efe0008d29e5f20d4a8e730435d8d2
Victor Colin Amador <vicolina@microsoft.com>
2020-02-21 18:28:58 -0800

    Refactored requestIdHeaderIsPopulated() in RequestIdInterceptorTest to make it easier to read.

--- sdk/core/azure-core/src/test/java/com/azure/android/core/http/interceptor/RequestIdInterceptorTest.java
+++ sdk/core/azure-core/src/test/java/com/azure/android/core/http/interceptor/RequestIdInterceptorTest.java
@@ -7,8 +7,6 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -17,39 +15,25 @@ import okhttp3.mockwebserver.MockWebServer;
 
 public class RequestIdInterceptorTest {
     @Rule
-    public final MockWebServer server = new MockWebServer();
+    public final MockWebServer mockWebServer = new MockWebServer(); // Server is started automatically
 
     @Test
-    public void requestIdHeaderIsPopulated() throws IOException {
-        server.enqueue(new MockResponse());
+    public void requestIdHeaderIsPopulated() throws IOException, InterruptedException {
+        mockWebServer.enqueue(new MockResponse());
 
-        final String[] requestId = {null};
-        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
-
-        httpClientBuilder.addInterceptor(new RequestIdInterceptor());
-        httpClientBuilder.addInterceptor(chain -> {
-            requestId[0] = (chain.request().header(HttpHeader.CLIENT_REQUEST_ID));
-
-            return chain.proceed(chain.request());
-        });
-
-        OkHttpClient httpClient = httpClientBuilder.build();
-        Request request = new okhttp3.Request.Builder()
-            .url(getEndpointFrom(server))
+        OkHttpClient okHttpClient = new OkHttpClient()
+            .newBuilder()
+            .addInterceptor(new RequestIdInterceptor())
+            .build();
+        Request request = new Request
+            .Builder()
+            .url(mockWebServer.url("/"))
             .build();
 
-        httpClient.newCall(request).execute();
+        okHttpClient.newCall(request).execute();
 
-        Assert.assertNotNull(requestId[0]);
-    }
+        String header = mockWebServer.takeRequest().getHeader(HttpHeader.CLIENT_REQUEST_ID);
 
-    private static URL getEndpointFrom(MockWebServer server) throws MalformedURLException {
-        try {
-            return new URL(String.format("http://%s:%s", server.getHostName(), server.getPort()));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-
-            throw e;
-        }
+        Assert.assertNotNull(header);
     }
 }
