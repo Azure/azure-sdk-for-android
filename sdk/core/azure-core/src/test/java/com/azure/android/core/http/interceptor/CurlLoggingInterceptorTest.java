87568136858adf2c56984af2f936edafc4b222ac
Victor Colin Amador <vicolina@microsoft.com>
2020-02-29 01:53:11 -0800

    Added unit tests for the CurlLoggingInterceptor.

--- sdk/core/azure-core/src/test/java/com/azure/android/core/http/interceptor/CurlLoggingInterceptorTest.java
+++ sdk/core/azure-core/src/test/java/com/azure/android/core/http/interceptor/CurlLoggingInterceptorTest.java
@@ -1,67 +1,197 @@
 package com.azure.android.core.http.interceptor;
 
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.List;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.mockwebserver.MockWebServer;
+
+import static com.azure.android.core.http.interceptor.TestUtils.buildOkHttpClientWithInterceptor;
+import static com.azure.android.core.http.interceptor.TestUtils.getSimpleRequest;
+import static com.azure.android.core.http.interceptor.TestUtils.getSimpleRequestWithHeader;
+import static com.azure.android.core.util.logging.ClientLogger.LOG_LEVEL_DEBUG;
+
 public class CurlLoggingInterceptorTest {
-    // Test that the CurlLoggingInterceptor only logs at debug level
-    /*
-    @Test
-    public void test() {
-    }
-    */
+    private final MockWebServer mockWebServer = new MockWebServer();
+    private final TestClientLogger testClientLogger = new TestClientLogger();
+    private final OkHttpClient okHttpClient =
+        buildOkHttpClientWithInterceptor(new CurlLoggingInterceptor(testClientLogger));
 
-    // Test that the CurlLoggingInterceptor does not redact headers or query string params
-    /*
-    @Test
-    public void test() {
-    }
-    */
+    @Rule
+    public EnqueueMockResponse enqueueMockResponse = new EnqueueMockResponse(mockWebServer);
 
-    // Test that the CurlLoggingInterceptor includes the method
-    /*
     @Test
-    public void test() {
-    }
-    */
+    public void curlLoggingInterceptor_logsAtDebugLevel() throws IOException {
+        // Given a request and a client with a CurlLoggingInterceptor.
 
-    // Test that the CurlLoggingInterceptor escapes quote marks in header values
-    /*
-    @Test
-    public void test() {
+        // When sending said request.
+        okHttpClient.newCall(getSimpleRequest(mockWebServer)).execute();
+
+        List<AbstractMap.SimpleEntry<Integer, String>> logs = testClientLogger.getLogs();
+
+        // Then all the messages should be logged at the DEBUG log level.
+        for (AbstractMap.SimpleEntry<Integer, String> log : logs) {
+            Assert.assertEquals(LOG_LEVEL_DEBUG, (long) log.getKey());
+        }
     }
-    */
+
+    @Test
+    public void headersAndQueryParameters_areNotRedacted() throws IOException {
+        // Given a request where at least one header is populated and there is at least one query parameter, and a
+        // client with a CurlLoggingInterceptor.
+        String testHeaderName = "Test-Header";
+        String testHeaderValue = "Test Value";
+        String urlWithQueryParam = "http://www.example.com/?testQueryParam=testValue";
+        Request request = new Request.Builder()
+            .addHeader(testHeaderName, testHeaderValue)
+            .url(mockWebServer.url(urlWithQueryParam))
+            .build();
+
+        // When sending said request.
+        okHttpClient.newCall(request).execute();
+
+        List<AbstractMap.SimpleEntry<Integer, String>> logs = testClientLogger.getLogs();
+        String curlCommand = logs.get(1).getValue();
+
+        // Then the cURL command should not redact the header and query parameter values.
+        Assert.assertTrue(curlCommand.contains("-H \"" + testHeaderName + ": " + testHeaderValue + "\""));
+        Assert.assertTrue(curlCommand.contains(urlWithQueryParam));
+    }
+
+    @Test
+    public void method_isIncluded() throws IOException {
+        // Given a request and a client with a CurlLoggingInterceptor.
+
+        // When sending said request.
+        okHttpClient.newCall(getSimpleRequest(mockWebServer)).execute();
+
+        List<AbstractMap.SimpleEntry<Integer, String>> logs = testClientLogger.getLogs();
+        String curlCommand = logs.get(1).getValue();
+
+        // Then the method executed should be logged at the DEBUG log level.
+        Assert.assertTrue(curlCommand.contains("-X GET"));
+    }
+
+    @Test
+    public void quotationMarksInHeaderValues_areProperlyEscaped() throws IOException {
+        // Given a request where a header value contains double quotation marks and a client with a
+        // CurlLoggingInterceptor.
+        String testHeaderName = "Test-Header";
+        String testHeaderValue = "Test Value";
+        Request request = getSimpleRequestWithHeader(mockWebServer, testHeaderName,"\"" + testHeaderValue + "\"");
+
+        // When sending said request.
+        okHttpClient.newCall(request).execute();
+
+        List<AbstractMap.SimpleEntry<Integer, String>> logs = testClientLogger.getLogs();
+        String curlCommand = logs.get(1).getValue();
+
+        // Then the quotation marks should be escaped in the cURL command.
+        Assert.assertTrue(curlCommand.contains("-H \"" + testHeaderName + ": \\\"" + testHeaderValue + "\\\"\""));
+    }
 
     // Test that the CurlLoggingInterceptor escapes backslashes in header values
-    /*
     @Test
-    public void test() {
-    }
-    */
+    public void backslashesInHeaderValues_areProperlyEscaped() throws IOException {
+        // Given a request where a header value contains backslashes and a client with a CurlLoggingInterceptor.
+        String testHeaderName = "Test-Header";
+        String testHeaderValue = "Test\\Value";
+        Request request = getSimpleRequestWithHeader(mockWebServer, testHeaderName, testHeaderValue);
 
-    // Test that the CurlLoggingInterceptor escapes single quotes in the body
-    /*
-    @Test
-    public void test() {
-    }
-    */
+        // When sending said request.
+        okHttpClient.newCall(request).execute();
 
-    // Test that the CurlLoggingInterceptor escapes newlines in the body
-    /*
-    @Test
-    public void test() {
-    }
-    */
+        List<AbstractMap.SimpleEntry<Integer, String>> logs = testClientLogger.getLogs();
+        String curlCommand = logs.get(1).getValue();
 
-    // Test that the CurlLoggingInterceptor flags compressed requests when the accept encoding is not identity
-    /*
-    @Test
-    public void test() {
+        // Then the backslashes should be escaped in the cURL command.
+        Assert.assertTrue(curlCommand.contains("-H \"" + testHeaderName + ": Test\\\\Value\""));
     }
-    */
+
+    @Test
+    public void singleQuotationMarksInBody_areProperlyEscaped() throws IOException {
+        // Given a request where a header value contains single quotation marks and a client with a
+        // CurlLoggingInterceptor.
+        Request request = new Request.Builder()
+            .put(RequestBody.create(MediaType.get("text/html"), "'Test body'"))
+            .url(mockWebServer.url("/"))
+            .build();
+
+        // When sending said request.
+        okHttpClient.newCall(request).execute();
+
+        List<AbstractMap.SimpleEntry<Integer, String>> logs = testClientLogger.getLogs();
+        String curlCommand = logs.get(1).getValue();
+
+        // Then the quotation marks should be escaped in the cURL command.
+        Assert.assertTrue(curlCommand.contains("\\'Test body\\'"));
+    }
+
+    @Test
+    public void newlineCharactersInBody_areProperlyEscaped() throws IOException {
+        // Given a request where a header value contains newline characters and a client with a CurlLoggingInterceptor.
+        Request request = new Request.Builder()
+            .put(RequestBody.create(MediaType.get("text/html"), "\nTest body\n"))
+            .url(mockWebServer.url("/"))
+            .build();
+
+        // When sending said request.
+        okHttpClient.newCall(request).execute();
+
+        List<AbstractMap.SimpleEntry<Integer, String>> logs = testClientLogger.getLogs();
+        String curlCommand = logs.get(1).getValue();
+
+        // Then the newline characters should be escaped in the cURL command.
+        Assert.assertTrue(curlCommand.contains("\\nTest body\\n"));
+    }
+
+    @Test
+    public void compressedFlagIsIncluded_whenContentEncodingIsNotIdentity() throws IOException {
+        // Given a request where a the Content-Encoding header does not have the 'identity' value and a client with a
+        // CurlLoggingInterceptor.
+        Request request = new Request.Builder()
+            .put(RequestBody.create(MediaType.get("text/html"), "Test body"))
+            .url(mockWebServer.url("/"))
+            .addHeader("Accept-Encoding", "gzip")
+            .build();
+
+        // When sending said request.
+        okHttpClient.newCall(request).execute();
+
+        List<AbstractMap.SimpleEntry<Integer, String>> logs = testClientLogger.getLogs();
+        String curlCommand = logs.get(1).getValue();
+
+        // Then the 'compressed' flag should be included in the cURL command.
+        Assert.assertTrue(curlCommand.contains("--compressed"));
+    }
 
     // Test that the CurlLoggingInterceptor omits the compressed flag requests when the accept encoding is
     // identity
-    /*
     @Test
-    public void test() {
+    public void compressedFlagIsNotIncluded_whenContentEncodingIsIdentity() throws IOException {
+        // Given a request where a the Content-Encoding header with the value 'identity' and a client with a
+        // CurlLoggingInterceptor.
+        Request request = new Request.Builder()
+            .put(RequestBody.create(MediaType.get("text/html"), "Test body"))
+            .url(mockWebServer.url("/"))
+            .addHeader("Content-Encoding", "identity")
+            .build();
+
+        // When sending said request.
+        okHttpClient.newCall(request).execute();
+
+        List<AbstractMap.SimpleEntry<Integer, String>> logs = testClientLogger.getLogs();
+        String curlCommand = logs.get(1).getValue();
+
+        // Then the 'compressed' flag should not be included in the cURL command.
+        Assert.assertFalse(curlCommand.contains("--compressed"));
     }
-    */
 }
