6015feff7b8b333a50e274d6c587feaf54e23ab7
Mitch Denny <midenn@microsoft.com>
2020-07-21 14:15:31 +1000

    Adds template project.

---
+++ sdk/template/azure-sdk-template/src/main/java/com/azure/android/template/Dummy.java
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.template;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+/**
+ * Dummy placeholder class
+ */
+public final class Dummy {
+    /**
+     * Creates a new {@link Dummy} object.
+     */
+    public Dummy) {
+    }
+
+    public static void doNothing() {
+    }
+}
