581e79e1f0b4be8b5ef2da552189105bdb17d04a
Sima Zhu <sizhu@microsoft.com>
2020-08-17 12:57:17 -0700

    test the default source branch

--- eng/common/scripts/Verify-Links.ps1
+++ eng/common/scripts/Verify-Links.ps1
@@ -173,19 +173,23 @@ function CheckLink ([System.Uri]$linkUri)
 }
 
 function ReplaceGithubLink([string]$originLink) {
+  Write-Verbose "what is in the branch input? $branchReplacementName"
   if (-not $branchReplacementName) {
     try {
       $branchReplacementName = $(system.pullRequest.sourceCommitId)
+      Write-Verbose "what is in the sourceCommit? $(system.pullRequest.sourceCommitId)"
       if (-not $branchReplacementName) {
-        $ReplacementPattern = "`${1}$branchReplacementName`$2"
-        return $originLink -replace $branchReplaceRegex, $ReplacementPattern 
+        return $originLink
       }
+      $ReplacementPattern = "`${1}$branchReplacementName`$2"
+      return $originLink -replace $branchReplaceRegex, $ReplacementPattern 
     } 
     catch {
       Write-Verbose "It is not triggered by pull request. Skip the replace link steps."
+      return $originLink
     }
   }
-  return $originLink
+  return $originLink -replace $branchReplaceRegex, "`${1}$branchReplacementName`$2" 
 }
 
 function GetLinks([System.Uri]$pageUri)
