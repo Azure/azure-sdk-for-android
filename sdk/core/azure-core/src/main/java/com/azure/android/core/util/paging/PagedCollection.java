285ac7227c6e19e659d7a38e12d87213c401252e
Victor Colin Amador <vicolina@microsoft.com>
2020-08-20 11:57:28 -0700

    Removed some member variables from PagedCollection's iterator. Refined the logic of hasNext() and when a new page should be retrieved.

--- sdk/core/azure-core/src/main/java/com/azure/android/core/util/paging/PagedCollection.java
+++ sdk/core/azure-core/src/main/java/com/azure/android/core/util/paging/PagedCollection.java
@@ -2,8 +2,8 @@ package com.azure.android.core.util.paging;
 
 import androidx.annotation.NonNull;
 
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 
 public class PagedCollection<T, P extends Page<T>> implements Iterable<T> {
     private static final int DEFAULT_PAGE_SIZE = 25; // Is this a good size?
@@ -27,37 +27,47 @@ public class PagedCollection<T, P extends Page<T>> implements Iterable<T> {
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
-            private boolean hasNext;
-            private int currentItem;
-            private List<T> items;
+            private Iterator<T> itemsIterator;
             private String itNextPageId;
 
             @Override
             public boolean hasNext() {
-                // If there are no items or the iterator is on the last item, get a page from the service.
-                if (items == null || currentItem == items.size() - 1) {
-                    pageRetriever.getPage(itNextPageId, DEFAULT_PAGE_SIZE, new PagingCallback<T, P>() {
-                        @Override
-                        public void onSuccess(P page, String currentPageId, String nextPageId) {
-                            items = page.getItems();
-                            currentItem = 0;
-                            itNextPageId = nextPageId;
-                            hasNext = items != null;
-                        }
-
-                        @Override
-                        public void onFailure(Throwable throwable) {
-                            throw new RuntimeException(throwable);
-                        }
-                    });
+                if (itemsIterator == null) {
+                    loadItems();
                 }
 
-                return hasNext;
+                if (itemsIterator.hasNext()) {
+                    return true;
+                } else {
+                    if (itNextPageId == null) {
+                        return false;
+                    } else {
+                        loadItems();
+
+                        return itemsIterator.hasNext();
+                    }
+                }
             }
 
             @Override
             public T next() {
-                return items.get(currentItem++);
+                return itemsIterator.next();
+            }
+
+            private void loadItems() {
+                pageRetriever.getPage(itNextPageId, DEFAULT_PAGE_SIZE, new PagingCallback<T, P>() {
+                    @Override
+                    public void onSuccess(P page, String currentPageId, String nextPageId) {
+                        itemsIterator =
+                            page.getItems() == null ? Collections.emptyIterator() : page.getItems().iterator();
+                        itNextPageId = nextPageId; // If null, this is the last page.
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        throw new RuntimeException(throwable);
+                    }
+                });
             }
         };
     }
