631949f8fc8a419cdaa0a4a9174af30c3ec7d98a
Jianping Chen <jp.chen.jianping@gmail.com>
2020-10-02 19:08:08 -0700

    Create initial version of Chat SDK for Android (#370)
    * Rebase from master after paging code merge
    
    * Remove page size from retriever constructors since it only makes it harder for autorest.android
    
    * Create preview version of Chat sdk for Android
    
    * Update with latest swagger
    
    * Add basic CRUD tests for thread, message, member
    
    * Remove sample test file
    
    * Update sdk/communication/chat/swagger/README.md
    
    Co-authored-by: vcolin7 <vicolina@microsoft.com>
    
    * Update sdk/communication/chat/build.gradle
    
    Co-authored-by: vcolin7 <vicolina@microsoft.com>
    
    * Fix android manifest for Chat
    
    * revert a merge error
    
    * revert a mistaken change to root gradle
    
    * revert a mistaken change to root gradle
    
    * Add unit tests for different paging collections; remove integration tests since the token will expire
    
    * Rename chat folder to azure-communication-chat; Update swagger README.md to link to swagger; Regenerate with swagger link
    
    * Update ci.yml to publish chat sdk
    
    * Minor changes in test code according to review feedback
    
    * Manually fix a codegen issue with parameter sequence
    
    * ACS Chat SDK Readme
    
    * Add change log
    
    * Update changelog for the release
    
    * Update chat gradle and changelog for correct version
    
    * Apply README review suggestions
    
    Co-authored-by: vcolin7 <vicolina@microsoft.com>
    Co-authored-by: Brandon Siegel <96068+bsiegel@users.noreply.github.com>
    
    * Update changelog
    
    * Readme: cleanup
    
    * removing locale from readme link
    
    * last touch for the changelog
    
    * README details and gradle comment should be consistent
    
    * README updates according to feedback
    
    Co-authored-by: JP Chen <jiach@microsoft.com>
    Co-authored-by: vcolin7 <vicolina@microsoft.com>
    Co-authored-by: Anu Thomas Chandy <anuamd@hotmail.com>
    Co-authored-by: Brandon Siegel <96068+bsiegel@users.noreply.github.com>

---
+++ sdk/communication/azure-communication-chat/src/test/java/com/azure/android/communication/chat/chatAsyncClientTest.java
@@ -0,0 +1,158 @@
+package com.azure.android.communication.chat;
+
+import com.azure.android.communication.chat.models.ChatMessage;
+import com.azure.android.communication.chat.models.ChatThreadInfo;
+import com.azure.android.core.http.Callback;
+import com.azure.android.core.http.ServiceClient;
+import com.azure.android.core.http.responsepaging.AsyncPagedDataCollection;
+import com.azure.android.core.util.paging.Page;
+
+import org.junit.After;
+import org.junit.Test;
+import org.threeten.bp.OffsetDateTime;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Response;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.fail;
+
+public class chatAsyncClientTest {
+
+    private static final MockWebServer mockWebServer = new MockWebServer();
+    private static final String BASE_URL = mockWebServer.url("/").toString();
+    private static AzureCommunicationChatServiceAsyncClient chatServiceAsyncClient =
+        new AzureCommunicationChatServiceAsyncClient.Builder()
+            .serviceClientBuilder(new ServiceClient.Builder().setBaseUrl(BASE_URL))
+            .build();
+
+    @After
+    public void tearDown() throws InterruptedException {
+        // For ensuring the responses enqueued are consumed before making the next call.
+        mockWebServer.takeRequest(20, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void listThreadPages() {
+        mockThreadsResponse(5);
+
+        chatServiceAsyncClient.listChatThreadsPages(5, OffsetDateTime.now(),new Callback<AsyncPagedDataCollection<ChatThreadInfo, Page<ChatThreadInfo>>>() {
+            @Override
+            public void onSuccess(AsyncPagedDataCollection<ChatThreadInfo, Page<ChatThreadInfo>> asyncPagedDataCollection, Response response) {
+                asyncPagedDataCollection.getFirstPage(new Callback<Page<ChatThreadInfo>>() {
+                    @Override
+                    public void onSuccess(Page<ChatThreadInfo> result, Response response) {
+                        assertEquals(5, result.getItems().size());
+                        mockThreadsResponse(3);
+
+                        asyncPagedDataCollection.getPage(result.getNextPageId(), new Callback<Page<ChatThreadInfo>>() {
+                            @Override
+                            public void onSuccess(Page<ChatThreadInfo> result, Response response) {
+                                assertEquals(3, result.getItems().size());
+
+                                asyncPagedDataCollection.getPage(result.getPreviousPageId(), new Callback<Page<ChatThreadInfo>>() {
+                                    @Override
+                                    public void onSuccess(Page<ChatThreadInfo> result, Response response) {
+                                        assertEquals(5, result.getItems().size());
+                                    }
+
+                                    @Override
+                                    public void onFailure(Throwable throwable, Response response) {
+                                        fail();
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onFailure(Throwable throwable, Response response) {
+                                fail();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable, Response response) {
+                        fail();
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Throwable throwable, Response response) {
+                fail();
+            }
+        });
+
+    }
+
+    @Test
+    public void listThreadMessagePages() {
+        mockMessagesResponse(5);
+
+        chatServiceAsyncClient.listChatMessagesPages("threadId", 5, OffsetDateTime.now(), new Callback<AsyncPagedDataCollection<ChatMessage, Page<ChatMessage>>>() {
+            @Override
+            public void onSuccess(AsyncPagedDataCollection<ChatMessage, Page<ChatMessage>> asyncPagedDataCollection, Response response) {
+                asyncPagedDataCollection.getFirstPage(new Callback<Page<ChatMessage>>() {
+                    @Override
+                    public void onSuccess(Page<ChatMessage> result, Response response) {
+                        assertEquals(5, result.getItems().size());
+                        mockMessagesResponse(3);
+                        asyncPagedDataCollection.getPage(result.getNextPageId(), new Callback<Page<ChatMessage>>() {
+                            @Override
+                            public void onSuccess(Page<ChatMessage> result, Response response) {
+                                assertEquals(3, result.getItems().size());
+
+                                asyncPagedDataCollection.getPage(result.getPreviousPageId(), new Callback<Page<ChatMessage>>() {
+                                    @Override
+                                    public void onSuccess(Page<ChatMessage> result, Response response) {
+                                        assertEquals(5, result.getItems().size());
+                                    }
+
+                                    @Override
+                                    public void onFailure(Throwable throwable, Response response) {
+                                        fail();
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onFailure(Throwable throwable, Response response) {
+                                fail();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable, Response response) {
+                        fail();
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Throwable throwable, Response response) {
+                fail();
+            }
+        });
+
+    }
+
+    private void mockThreadsResponse(int n) {
+        MockResponse mockResponse = new MockResponse()
+            .setResponseCode(200)
+            .setBody(ThreadsMocker.mockThreads(n));
+        mockWebServer.enqueue(mockResponse);
+    }
+
+    private void mockMessagesResponse(int n) {
+        String body = MessagesMocker.mockThreadMessages(n);
+        MockResponse mockResponse = new MockResponse()
+            .setResponseCode(200)
+            .setBody(body);
+        mockWebServer.enqueue(mockResponse);
+    }
+}
