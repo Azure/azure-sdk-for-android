7e79d47151f04fedcdbf853e5383bccc2359859f
Anu Thomas Chandy <anuamd@hotmail.com>
2021-01-08 13:54:37 -0800

    Enabling callback based HttpLoggingPolicy and tests

---
+++ sdk/core/azure-core-http/src/main/java/com/azure/android/core/http/policy/HttpLoggingPolicy.java
@@ -0,0 +1,321 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.core.http.policy;
+
+import com.azure.android.core.http.HttpCallback;
+import com.azure.android.core.http.HttpHeader;
+import com.azure.android.core.http.HttpHeaders;
+import com.azure.android.core.http.HttpPipelinePolicy;
+import com.azure.android.core.http.HttpPipelinePolicyChain;
+import com.azure.android.core.http.HttpRequest;
+import com.azure.android.core.http.HttpResponse;
+import com.azure.android.core.http.implementation.Util;
+import com.azure.android.core.http.util.UrlBuilder;
+import com.azure.core.http.ContentType;
+import com.azure.core.logging.ClientLogger;
+import com.azure.core.logging.LogLevel;
+import com.azure.core.micro.util.Context;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+/**
+ * The pipeline policy that handles logging of HTTP requests and responses.
+ */
+public class HttpLoggingPolicy implements HttpPipelinePolicy {
+    private static final int MAX_BODY_LOG_SIZE = 1024 * 16;
+    private static final String REDACTED_PLACEHOLDER = "REDACTED";
+
+    private final HttpLogDetailLevel httpLogDetailLevel;
+    private final Set<String> allowedHeaderNames;
+    private final Set<String> allowedQueryParameterNames;
+
+    /**
+     * Key for {@link Context} to pass request retry count metadata for logging.
+     */
+    public static final String RETRY_COUNT_CONTEXT = "requestRetryCount";
+
+    /**
+     * Creates an HttpLoggingPolicy with the given log configurations.
+     *
+     * @param httpLogOptions The HTTP logging configuration options.
+     */
+    public HttpLoggingPolicy(HttpLogOptions httpLogOptions) {
+        if (httpLogOptions == null) {
+            this.httpLogDetailLevel = HttpLogDetailLevel.NONE;
+            this.allowedHeaderNames = Collections.emptySet();
+            this.allowedQueryParameterNames = Collections.emptySet();
+        } else {
+            this.httpLogDetailLevel = httpLogOptions.getLogLevel();
+            this.allowedHeaderNames = httpLogOptions.getAllowedHeaderNames()
+                .stream()
+                .map(headerName -> headerName.toLowerCase(Locale.ROOT))
+                .collect(Collectors.toSet());
+            this.allowedQueryParameterNames = httpLogOptions.getAllowedQueryParamNames()
+                .stream()
+                .map(queryParamName -> queryParamName.toLowerCase(Locale.ROOT))
+                .collect(Collectors.toSet());
+            // this.prettyPrintBody = httpLogOptions.isPrettyPrintBody();
+        }
+    }
+
+    @Override
+    public void process(HttpPipelinePolicyChain chain) {
+        // No logging will be performed, trigger a no-op.
+        if (httpLogDetailLevel == HttpLogDetailLevel.NONE) {
+            chain.processNextPolicy(chain.getRequest());
+            return;
+        }
+
+        // final ClientLogger logger = new ClientLogger((String) context.getData("caller-method").orElse(""));
+        final ClientLogger logger = new ClientLogger((String) "caller-method"); // TODO: bring context ^
+        if (!logger.canLogAtLevel(LogLevel.INFORMATIONAL)) {
+            chain.processNextPolicy(chain.getRequest());
+            return;
+        }
+
+        final long startNs = System.nanoTime();
+        final HttpRequest httpRequest = chain.getRequest();
+        // final Integer retryCount = context.getData(RETRY_COUNT_CONTEXT);
+        // final Integer retryCount = null; // TODO: bring context ^
+
+        StringBuilder requestLogMessage = new StringBuilder();
+        if (httpLogDetailLevel.shouldLogUrl()) {
+            requestLogMessage.append("--> ")
+                .append(httpRequest.getHttpMethod())
+                .append(" ")
+                .append(this.getRedactedUrl(httpRequest.getUrl()))
+                .append(System.lineSeparator());
+
+            // if (retryCount != null) {
+            //     requestLogMessage.append(retryCount).append(System.lineSeparator());
+            // }
+        }
+
+        this.appendHeaders(logger, httpRequest.getHeaders(), requestLogMessage);
+
+        if (httpLogDetailLevel.shouldLogBody()) {
+            if (httpRequest.getBody() == null) {
+                requestLogMessage.append("(empty body)")
+                    .append(System.lineSeparator())
+                    .append("--> END ")
+                    .append(httpRequest.getHttpMethod())
+                    .append(System.lineSeparator());
+            } else {
+                final String requestContentType = httpRequest.getHeaders().getValue("Content-Type");
+                final long requestContentLength = this.getContentLength(logger, httpRequest.getHeaders());
+                if (this.isContentLoggable(requestContentType, requestContentLength)) {
+                    final String content = this.convertBytesToString(httpRequest.getBody(), logger);
+                    requestLogMessage.append(requestContentLength)
+                        .append("-byte body:")
+                        .append(System.lineSeparator())
+                        .append(content)
+                        .append(System.lineSeparator())
+                        .append("--> END ")
+                        .append(httpRequest.getHttpMethod())
+                        .append(System.lineSeparator());
+                } else {
+                    requestLogMessage.append(requestContentLength)
+                        .append("-byte body: (content not logged)")
+                        .append(System.lineSeparator())
+                        .append("--> END ")
+                        .append(httpRequest.getHttpMethod())
+                        .append(System.lineSeparator());
+                }
+            }
+        }
+
+        logger.info(requestLogMessage.toString());
+
+        chain.processNextPolicy(httpRequest, new HttpCallback() {
+            @Override
+            public void onSuccess(HttpResponse response) {
+                long tookMs = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs);
+
+                final String contentLengthHeaderValue = response.getHeaderValue("Content-Length");
+                final String contentLengthMessage = (Util.isNullOrEmpty(contentLengthHeaderValue))
+                    ? "unknown-length body"
+                    : contentLengthHeaderValue + "-byte body";
+
+                StringBuilder responseLogMessage = new StringBuilder();
+                if (httpLogDetailLevel.shouldLogUrl()) {
+                    responseLogMessage.append("<-- ")
+                        .append(response.getStatusCode())
+                        .append(" ")
+                        .append(getRedactedUrl(response.getRequest().getUrl()))
+                        .append(" (")
+                        .append(tookMs)
+                        .append(" ms, ")
+                        .append(contentLengthMessage)
+                        .append(")")
+                        .append(System.lineSeparator());
+                }
+
+                appendHeaders(logger, response.getHeaders(), responseLogMessage);
+
+                HttpResponse httpResponse = response;
+                if (httpLogDetailLevel.shouldLogBody()) {
+                    final String responseContentType = response.getHeaderValue("Content-Type");
+                    final long responseContentLength = getContentLength(logger, response.getHeaders());
+                    if (isContentLoggable(responseContentType, responseContentLength)) {
+                        httpResponse = response.buffer();
+                        final String content = convertBytesToString(httpResponse.getBodyAsByteArray(), logger);
+                        responseLogMessage.append("Response body:")
+                            .append(System.lineSeparator())
+                            .append(content)
+                            .append(System.lineSeparator())
+                            .append("<-- END HTTP");
+                    } else {
+                        responseLogMessage.append("(body content not logged)")
+                            .append(System.lineSeparator())
+                            .append("<-- END HTTP");
+                    }
+                } else {
+                    responseLogMessage.append("<-- END HTTP");
+                }
+                logger.info(responseLogMessage.toString());
+                chain.finishedProcessing(httpResponse);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                logger.warning("<-- HTTP FAILED: ", error);
+                chain.finishedProcessing(error);
+            }
+        });
+    }
+
+    /*
+     * Generates the redacted URL for logging.
+     *
+     * @param url URL where the request is being sent.
+     * @return A URL with query parameters redacted based on configurations in this policy.
+     */
+    private String getRedactedUrl(URL url) {
+        return UrlBuilder.parse(url)
+            .setQuery(this.getAllowedQueryString(url.getQuery()))
+            .toString();
+    }
+
+    /*
+     * Generates the logging safe query parameters string.
+     *
+     * @param queryString Query parameter string from the request URL.
+     * @return A query parameter string redacted based on the configurations in this policy.
+     */
+    private String getAllowedQueryString(String queryString) {
+        if (Util.isNullOrEmpty(queryString)) {
+            return "";
+        }
+
+        StringBuilder queryStringBuilder = new StringBuilder();
+        String[] queryParams = queryString.split("&");
+        for (String queryParam : queryParams) {
+            if (queryStringBuilder.length() > 0) {
+                queryStringBuilder.append("&");
+            }
+
+            String[] queryPair = queryParam.split("=", 2);
+            if (queryPair.length == 2) {
+                String queryName = queryPair[0];
+                if (allowedQueryParameterNames.contains(queryName.toLowerCase(Locale.ROOT))) {
+                    queryStringBuilder.append(queryParam);
+                } else {
+                    queryStringBuilder.append(queryPair[0]).append("=").append(REDACTED_PLACEHOLDER);
+                }
+            } else {
+                queryStringBuilder.append(queryParam);
+            }
+        }
+
+        return queryStringBuilder.toString();
+    }
+
+    /*
+     * Adds HTTP headers into the StringBuilder that is generating the log message.
+     *
+     * @param headers HTTP headers on the request or response.
+     * @param sb StringBuilder that is generating the log message.
+     * @param logLevel Log level the environment is configured to use.
+     */
+    private void appendHeaders(ClientLogger logger, HttpHeaders headers, StringBuilder logMessage) {
+        // Either headers shouldn't be logged or the logging level isn't set to VERBOSE, don't add headers.
+        if (!httpLogDetailLevel.shouldLogHeaders() || !logger.canLogAtLevel(LogLevel.VERBOSE)) {
+            return;
+        }
+
+        for (HttpHeader header : headers) {
+            String headerName = header.getName();
+            logMessage.append(headerName).append(":");
+            if (allowedHeaderNames.contains(headerName.toLowerCase(Locale.ROOT))) {
+                logMessage.append(header.getValue());
+            } else {
+                logMessage.append(REDACTED_PLACEHOLDER);
+            }
+            logMessage.append(System.lineSeparator());
+        }
+    }
+
+    /*
+     * Attempts to retrieve and parse the Content-Length header into a numeric representation.
+     *
+     * @param logger Logger used to log a warning if the Content-Length header is an invalid number.
+     * @param headers HTTP headers that are checked for containing Content-Length.
+     * @return The retrieved content length.
+     */
+    private long getContentLength(ClientLogger logger, HttpHeaders headers) {
+        long contentLength = 0;
+
+        String contentLengthString = headers.getValue("Content-Length");
+        if (Util.isNullOrEmpty(contentLengthString)) {
+            return contentLength;
+        }
+
+        try {
+            contentLength = Long.parseLong(contentLengthString);
+        } catch (NumberFormatException | NullPointerException e) {
+            logger.warning("Could not parse the HTTP header content-length: '{}'.",
+                headers.getValue("content-length"), e);
+        }
+
+        return contentLength;
+    }
+
+    /*
+     * Determines if the request or response body should be logged.
+     *
+     * <p>The request or response body is logged if the Content-Type is not "application/octet-stream" and the body
+     * isn't empty and is less than 16KB in size.</p>
+     *
+     * @param contentTypeHeader Content-Type header value.
+     * @param contentLength Content-Length header represented as a numeric.
+     * @return A flag indicating if the request or response body should be logged.
+     */
+    private boolean isContentLoggable(String contentTypeHeader, long contentLength) {
+        return !ContentType.APPLICATION_OCTET_STREAM.equalsIgnoreCase(contentTypeHeader)
+            && contentLength != 0
+            && contentLength < MAX_BODY_LOG_SIZE;
+    }
+
+    /*
+     * Converts a byte array to a String.
+     *
+     * @param bytes The bytes to convert.
+     * @param logger The logger to log if conversion fails.
+     * @return The byte array as string.
+     *
+     */
+    private String convertBytesToString(byte[] bytes, ClientLogger logger) {
+        try {
+            return new String(bytes, "UTF-8");
+        } catch (UnsupportedEncodingException ex) {
+            throw logger.logExceptionAsError(new RuntimeException(ex));
+        }
+    }
+}
---
+++ sdk/core/azure-core-http/src/test/java/com/azure/android/core/http/policy/HttpLoggingPolicyTests.java
@@ -0,0 +1,324 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.core.http.policy;
+
+import com.azure.android.core.http.HttpCallDispatcher;
+import com.azure.android.core.http.HttpCallback;
+import com.azure.android.core.http.HttpClient;
+import com.azure.android.core.http.HttpHeaders;
+import com.azure.android.core.http.HttpMethod;
+import com.azure.android.core.http.HttpPipeline;
+import com.azure.android.core.http.HttpPipelineBuilder;
+import com.azure.android.core.http.HttpRequest;
+import com.azure.android.core.http.HttpResponse;
+import com.azure.android.core.test.http.MockHttpResponse;
+import com.azure.android.core.test.http.NoOpHttpClient;
+import com.azure.core.http.ContentType;
+import com.azure.core.micro.util.Context;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.parallel.ResourceLock;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * This class contains tests for {@link HttpLoggingPolicy}.
+ */
+public class HttpLoggingPolicyTests {
+    private static final String REDACTED = "REDACTED";
+    private static final Context CONTEXT = new Context("caller-method", HttpLoggingPolicyTests.class.getName());
+
+    private PrintStream originalSystemOut;
+    private ByteArrayOutputStream logCaptureStream;
+
+    @BeforeEach
+    public void prepareForTest() {
+        /*
+         * DefaultLogger uses System.out to log. Inject a custom PrintStream to log into for the duration of the test to
+         * capture the log messages.
+         */
+        originalSystemOut = System.out;
+        logCaptureStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(logCaptureStream));
+    }
+
+    @AfterEach
+    public void cleanupAfterTest() throws IOException {
+        // Reset System.err to the original PrintStream.
+        System.setOut(originalSystemOut);
+        logCaptureStream.close();
+    }
+
+    /**
+     * Tests that a query string will be properly redacted before it is logged.
+     */
+    @ParameterizedTest
+    @MethodSource("redactQueryParametersSupplier")
+    @ResourceLock("SYSTEM_OUT")
+    public void redactQueryParameters(String requestUrl, String expectedQueryString,
+        Set<String> allowedQueryParameters) {
+        HttpPipeline pipeline = new HttpPipelineBuilder()
+            .policies(new HttpLoggingPolicy(new HttpLogOptions()
+                .setLogLevel(HttpLogDetailLevel.BASIC)
+                .setAllowedQueryParamNames(allowedQueryParameters)))
+            .httpClient(new NoOpHttpClient())
+            .build();
+
+        CountDownLatch latch = new CountDownLatch(1);
+        // pipeline.send(new HttpRequest(HttpMethod.POST, requestUrl), CONTEXT, new HttpCallback() {..})
+        // TODO: enable context for HttpPipeline.send ^
+        pipeline.send(new HttpRequest(HttpMethod.POST, requestUrl), new HttpCallback() {
+            @Override
+            public void onSuccess(HttpResponse response) {
+                latch.countDown();
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                try {
+                    assertTrue(false, "unexpected call to pipeline::send onError" + error.getMessage());
+                } finally {
+                    latch.countDown();
+                }
+            }
+        });
+        awaitOnLatch(latch, "redactQueryParameters");
+
+        assertTrue(convertOutputStreamToString(logCaptureStream).contains(expectedQueryString));
+    }
+
+    private static Stream<Arguments> redactQueryParametersSupplier() {
+        String requestUrl = "https://localhost?sensitiveQueryParameter=sensitiveValue&queryParameter=value";
+
+        String expectedFormat = "sensitiveQueryParameter=%s&queryParameter=%s";
+        String fullyRedactedQueryString = String.format(expectedFormat, REDACTED, REDACTED);
+        String sensitiveRedactionQueryString = String.format(expectedFormat, REDACTED, "value");
+        String fullyAllowedQueryString = String.format(expectedFormat, "sensitiveValue", "value");
+
+        Set<String> allQueryParameters = new HashSet<>();
+        allQueryParameters.add("sensitiveQueryParameter");
+        allQueryParameters.add("queryParameter");
+
+        return Stream.of(
+            // All query parameters should be redacted.
+            Arguments.of(requestUrl, fullyRedactedQueryString, new HashSet<String>()),
+
+            // Only the sensitive query parameter should be redacted.
+            Arguments.of(requestUrl, sensitiveRedactionQueryString, Collections.singleton("queryParameter")),
+
+            // No query parameters are redacted.
+            Arguments.of(requestUrl, fullyAllowedQueryString, allQueryParameters)
+        );
+    }
+
+    /**
+     * Tests that logging the request body doesn't consume the stream before it is sent over the network.
+     */
+    @ParameterizedTest(name = "[{index}] {displayName}")
+    @MethodSource("validateLoggingDoesNotConsumeSupplier")
+    @ResourceLock("SYSTEM_OUT")
+    public void validateLoggingDoesNotChangeRequest(byte[] content, byte[] data, int contentLength)
+        throws MalformedURLException {
+        URL requestUrl = new URL("https://test.com");
+        HttpHeaders requestHeaders = new HttpHeaders()
+            .put("Content-Type", ContentType.APPLICATION_JSON)
+            .put("Content-Length", Integer.toString(contentLength));
+
+        HttpPipeline pipeline = new HttpPipelineBuilder()
+            .policies(new HttpLoggingPolicy(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY)))
+            .httpClient(new HttpClient() {
+                @Override
+                public HttpCallDispatcher getHttpCallDispatcher() {
+                    return new HttpCallDispatcher();
+                }
+
+                @Override
+                public void send(HttpRequest httpRequest, HttpCallback httpCallback) {
+                    assertArrayEquals(data, httpRequest.getBody());
+                    httpCallback.onSuccess(new MockHttpResponse(httpRequest, 200));
+                }
+            })
+            .build();
+
+        CountDownLatch latch = new CountDownLatch(1);
+        // pipeline.send(new HttpRequest(HttpMethod.POST, requestUrl, requestHeaders, content), CONTEXT)
+        // TODO: enable context for HttpPipeline.send ^
+        pipeline.send(new HttpRequest(HttpMethod.POST, requestUrl, requestHeaders, content), new HttpCallback() {
+            @Override
+            public void onSuccess(HttpResponse response) {
+                latch.countDown();
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                try {
+                    assertTrue(false, "unexpected call to pipeline::send onError" + error.getMessage());
+                } finally {
+                    latch.countDown();
+                }
+            }
+        });
+        awaitOnLatch(latch, "validateLoggingDoesNotChangeRequest");
+
+        String logString = convertOutputStreamToString(logCaptureStream);
+        assertTrue(logString.contains(new String(data, StandardCharsets.UTF_8)));
+    }
+
+    /**
+     * Tests that logging the response body doesn't consume the stream before it is returned from the service call.
+     */
+    @ParameterizedTest(name = "[{index}] {displayName}")
+    @MethodSource("validateLoggingDoesNotConsumeSupplier")
+    @ResourceLock("SYSTEM_OUT")
+    public void validateLoggingDoesNotChangeResponse(byte[] content, byte[] data, int contentLength) {
+        HttpRequest request = new HttpRequest(HttpMethod.GET, "https://test.com");
+        HttpHeaders responseHeaders = new HttpHeaders()
+            .put("Content-Type", ContentType.APPLICATION_JSON)
+            .put("Content-Length", Integer.toString(contentLength));
+
+        HttpPipeline pipeline = new HttpPipelineBuilder()
+            .policies(new HttpLoggingPolicy(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY)))
+            .httpClient(new HttpClient() {
+                @Override
+                public HttpCallDispatcher getHttpCallDispatcher() {
+                    return new HttpCallDispatcher();
+                }
+
+                @Override
+                public void send(HttpRequest httpRequest, HttpCallback httpCallback) {
+                    httpCallback.onSuccess(new MockHttpResponse(httpRequest, 200, responseHeaders, content));
+                }
+            })
+            .build();
+
+        CountDownLatch latch = new CountDownLatch(1);
+        // pipeline.send(request, CONTEXT)
+        // TODO: enable context for HttpPipeline.send ^
+        pipeline.send(request, new HttpCallback() {
+            @Override
+            public void onSuccess(HttpResponse response) {
+                try {
+                    assertArrayEquals(data, response.getBodyAsByteArray());
+                } finally {
+                    latch.countDown();
+                }
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                try {
+                    assertTrue(false, "unexpected call to pipeline::send onError" + error.getMessage());
+                } finally {
+                    latch.countDown();
+                }
+            }
+        });
+
+        awaitOnLatch(latch, "validateLoggingDoesNotChangeResponse");
+        String logString = convertOutputStreamToString(logCaptureStream);
+        assertTrue(logString.contains(new String(data, StandardCharsets.UTF_8)));
+    }
+
+    private static Stream<Arguments> validateLoggingDoesNotConsumeSupplier() {
+        byte[] data = "this is a test".getBytes(StandardCharsets.UTF_8);
+        byte[] repeatingData = new byte[data.length * 3];
+        for (int i = 0; i < 3; i++) {
+            System.arraycopy(data, 0, repeatingData, i * data.length, data.length);
+        }
+
+        return Stream.of(
+            Arguments.of(data, data, data.length)
+        );
+    }
+
+//    @ParameterizedTest(name = "[{index}] {displayName}")
+//    @EnumSource(value = HttpLogDetailLevel.class, mode = EnumSource.Mode.INCLUDE,
+//        names = { "BODY_AND_HEADERS" })
+//    @ResourceLock("SYSTEM_OUT")
+//    public void loggingIncludesRetryCount(HttpLogDetailLevel logLevel) {
+//        AtomicInteger requestCount = new AtomicInteger();
+//        HttpRequest request = new HttpRequest(HttpMethod.GET, "https://test.com");
+//
+//        HttpPipeline pipeline = new HttpPipelineBuilder()
+//            .policies(RetryPolicy.withFixedDelay(2, Duration.ofSeconds(1)),
+//                new HttpLoggingPolicy(new HttpLogOptions().setLogLevel(logLevel)))
+//            .httpClient(new HttpClient() {
+//                @Override
+//                public HttpCallDispatcher getHttpCallDispatcher() {
+//                    return new HttpCallDispatcher();
+//                }
+//
+//                @Override
+//                public void send(HttpRequest httpRequest, HttpCallback httpCallback) {
+//                    if (requestCount.getAndIncrement() == 0) {
+//                        httpCallback.onError(new RuntimeException("Try again!"));
+//                    } else {
+//                        httpCallback.onSuccess(new MockHttpResponse(httpRequest, 200));
+//                    }
+//                }
+//            })
+//            .build();
+//
+//        CountDownLatch latch = new CountDownLatch(1);
+//        // pipeline.send(request, CONTEXT)
+//        // TODO: enable context for HttpPipeline.send ^
+//        pipeline.send(request, new HttpCallback() {
+//            @Override
+//            public void onSuccess(HttpResponse response) {
+//                try {
+//                    assertEquals(200, response.getStatusCode());
+//                } finally {
+//                    latch.countDown();
+//                }
+//            }
+//
+//            @Override
+//            public void onError(Throwable error) {
+//                latch.countDown();
+//            }
+//        });
+//
+//        awaitOnLatch(latch, "loggingIncludesRetryCount");
+//
+//        String logString = convertOutputStreamToString(logCaptureStream);
+//        assertTrue(logString.contains("Try count: 1"));
+//        assertTrue(logString.contains("Try count: 2"));
+//    }
+
+    private static String convertOutputStreamToString(ByteArrayOutputStream stream) {
+        try {
+            return stream.toString("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void awaitOnLatch(CountDownLatch latch, String method) {
+        try {
+            latch.await(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            assertFalse(true, method + " didn't produce any result.");
+        }
+    }
+}
