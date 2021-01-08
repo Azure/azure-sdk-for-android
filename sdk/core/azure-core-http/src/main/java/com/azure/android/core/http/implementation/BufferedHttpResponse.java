1df804518a456103dd3931cb12176d5657221bf6
Anu Thomas Chandy <anuamd@hotmail.com>
2021-01-08 13:51:08 -0800

    BufferedHttpResponse: proper caching of response content

--- sdk/core/azure-core-http/src/main/java/com/azure/android/core/http/implementation/BufferedHttpResponse.java
+++ sdk/core/azure-core-http/src/main/java/com/azure/android/core/http/implementation/BufferedHttpResponse.java
@@ -7,7 +7,7 @@ import com.azure.android.core.http.HttpHeaders;
 import com.azure.android.core.http.HttpResponse;
 import com.azure.core.logging.ClientLogger;
 
-import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,7 +20,9 @@ public final class BufferedHttpResponse extends HttpResponse {
     private final ClientLogger logger = new ClientLogger(BufferedHttpResponse.class);
 
     private final HttpResponse innerHttpResponse;
-    private final BufferedInputStream bufferedBody;
+    private byte[] bufferedContent;
+    private volatile boolean closed;
+
 
     /**
      * Creates a buffered HTTP response.
@@ -30,48 +32,51 @@ public final class BufferedHttpResponse extends HttpResponse {
     public BufferedHttpResponse(HttpResponse innerHttpResponse) {
         super(innerHttpResponse.getRequest());
         this.innerHttpResponse = innerHttpResponse;
-        this.bufferedBody = new BufferedInputStream(this.innerHttpResponse.getBody());
     }
 
     @Override
     public int getStatusCode() {
-        return innerHttpResponse.getStatusCode();
+        return this.innerHttpResponse.getStatusCode();
     }
 
     @Override
     public String getHeaderValue(String name) {
-        return innerHttpResponse.getHeaderValue(name);
+        return this.innerHttpResponse.getHeaderValue(name);
     }
 
     @Override
     public HttpHeaders getHeaders() {
-        return innerHttpResponse.getHeaders();
+        return this.innerHttpResponse.getHeaders();
     }
 
     @Override
     public InputStream getBody() {
-        return bufferedBody;
+        return new ByteArrayInputStream(this.getBodyAsByteArray());
     }
 
     @Override
-    public byte[] getBodyAsByteArray() {
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int len;
-        try {
-            while ((len = this.bufferedBody.read(buffer)) != -1) {
-                os.write(buffer, 0, len);
+    public synchronized byte[] getBodyAsByteArray() {
+        if (this.bufferedContent == null) {
+            InputStream innerStream = this.innerHttpResponse.getBody();
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len;
+            try {
+                while ((len = innerStream.read(buffer)) != -1) {
+                    outStream.write(buffer, 0, len);
+                }
+                this.bufferedContent = outStream.toByteArray();
+            } catch (IOException ioe) {
+                throw logger.logExceptionAsError(new RuntimeException(ioe));
             }
-            return os.toByteArray();
-        } catch (IOException ioe) {
-            throw logger.logExceptionAsError(new RuntimeException(ioe));
         }
+        return this.bufferedContent;
     }
 
     @Override
     public String getBodyAsString() {
         return Util.bomAwareToString(this.getBodyAsByteArray(),
-            innerHttpResponse.getHeaderValue("Content-Type"));
+            this.innerHttpResponse.getHeaderValue("Content-Type"));
     }
 
     @Override
@@ -82,5 +87,14 @@ public final class BufferedHttpResponse extends HttpResponse {
     @Override
     public BufferedHttpResponse buffer() {
         return this;
+    }
+
+    @Override
+    public void close() {
+        if (!this.closed) {
+            this.innerHttpResponse.close();
+            this.closed = true;
+            super.close();
+        }
     }
 }
