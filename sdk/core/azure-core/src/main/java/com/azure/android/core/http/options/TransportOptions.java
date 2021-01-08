9b410438f2b939419d99bdfd172d489f5cc69e9f
Victor Colin Amador <victor.y.asi@gmail.com>
2021-01-08 15:11:56 -0600

    Added ClientOptions, RequestOptions and TransportOptions classes to azure-core.

---
+++ sdk/core/azure-core/src/main/java/com/azure/android/core/http/options/TransportOptions.java
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.core.http.options;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.azure.android.core.http.ServiceClient;
+import com.azure.android.core.http.interceptor.RetryStrategy;
+
+import org.threeten.bp.Duration;
+
+/**
+ * Options for configuring calls made by a {@link ServiceClient}.
+ */
+public class TransportOptions {
+    @NonNull
+    private final Duration timeout;
+    @Nullable
+    private final RetryStrategy retryStrategy;
+
+    /**
+     * Creates an instance of {@link TransportOptions}.
+     *
+     * @param timeout       Default timeout on any network call. 0 (zero) means there is no timeout.
+     * @param retryStrategy The {@link RetryStrategy} to be used for calls made by the {@link ServiceClient}.
+     */
+    public TransportOptions(@Nullable Duration timeout, @Nullable RetryStrategy retryStrategy) {
+        this.timeout = timeout == null ? Duration.ZERO : timeout;
+        this.retryStrategy = retryStrategy;
+    }
+
+    /**
+     * Gets the default timeout for calls made by the {@link ServiceClient}.
+     *
+     * @return the default timeout in seconds. 0 (zero) means there is no timeout.
+     */
+    @NonNull
+    public Duration getTimeout() {
+        return timeout;
+    }
+
+    /**
+     * Gets the {@link RetryStrategy} to be used for calls made by the {@link ServiceClient}.
+     *
+     * @return The {@link RetryStrategy}.
+     */
+    @Nullable
+    public RetryStrategy getRetryStrategy() {
+        return retryStrategy;
+    }
+}
