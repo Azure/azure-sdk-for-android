442f2c96b91128e33b1cc3d599fc73df35af8a69
JP Chen <jiach@microsoft.com>
2021-01-14 11:56:24 -0800

    Rename id to userId in TeamsUserIdentifier

--- sdk/communication/azure-communication-common/src/main/java/com/azure/android/communication/common/MicrosoftTeamsUserIdentifier.java
+++ sdk/communication/azure-communication-common/src/main/java/com/azure/android/communication/common/MicrosoftTeamsUserIdentifier.java
@@ -4,17 +4,17 @@ package com.azure.android.communication.common;
  * Represents a Microsoft Teams user.
  */
 public class MicrosoftTeamsUserIdentifier extends CommunicationIdentifier {
-    private final String id;
+    private final String userId;
     private final boolean isAnonymous;
 
     /**
      * Creates a {@link MicrosoftTeamsUserIdentifier} from the provided identifier string.
      * <p>
-     * The same identifier string will be returned whenever {@link #getId()} is called.
+     * The same identifier string will be returned whenever {@link #getUserId()} is called.
      * @param teamsUserId Teams user identifier string
      */
     public MicrosoftTeamsUserIdentifier(String teamsUserId) {
-        id = teamsUserId;
+        userId = teamsUserId;
         isAnonymous = false;
     }
 
@@ -22,21 +22,21 @@ public class MicrosoftTeamsUserIdentifier extends CommunicationIdentifier {
      * Creates a {@link MicrosoftTeamsUserIdentifier} from the provided identifier string.
      * Optionally specify whether this is an anonymous user.
      * <p>
-     * The same identifier string will be returned whenever {@link #getId()} is called.
+     * The same identifier string will be returned whenever {@link #getUserId()} is called.
      *
      * @param teamsUserId Teams user identifier string
      * @param isAnonymous Set this to true if the user is anonymous, for example when joining a meeting with a share link.
      */
     public MicrosoftTeamsUserIdentifier(String teamsUserId, boolean isAnonymous) {
-        id = teamsUserId;
+        userId = teamsUserId;
         this.isAnonymous = isAnonymous;
     }
 
     /**
      * The identifier of the Microsoft Teams user. If the user isn't anonymous, the id is the AAD object id of the user.
      */
-    public String getId() {
-        return id;
+    public String getUserId() {
+        return userId;
     }
 
     /**
