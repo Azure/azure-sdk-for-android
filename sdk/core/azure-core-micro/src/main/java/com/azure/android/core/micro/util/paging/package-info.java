fa05841ea649f17b455f08fba786d95ef7ff56a4
Anu Thomas Chandy <anuamd@hotmail.com>
2021-02-01 08:30:22 -0800

    Adding paging package with ContinuablePage (the base page type) to core module

---
+++ sdk/core/azure-core-micro/src/main/java/com/azure/android/core/micro/util/paging/package-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/**
+ * Package containing paging abstraction.
+ */
+package com.azure.android.core.micro.util.paging;
