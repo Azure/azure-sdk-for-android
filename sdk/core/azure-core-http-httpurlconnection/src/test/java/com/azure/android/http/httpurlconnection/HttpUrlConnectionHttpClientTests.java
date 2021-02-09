b303efedfa94e7f7b0b04d17c5c1ecb525d0be6b
Anu Thomas Chandy <anuamd@hotmail.com>
2021-02-08 16:34:51 -0800

    Adding BOM header tests to azure-core-test and enabling same tests for OkHttp and HttpUrlConnection HttpClient impls

---
+++ sdk/core/azure-core-http-httpurlconnection/src/test/java/com/azure/android/http/httpurlconnection/HttpUrlConnectionHttpClientTests.java
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.http.httpurlconnection;
+
+import com.azure.android.core.http.HttpClient;
+import com.azure.android.core.http.httpurlconnection.HttpUrlConnectionAsyncHttpClientBuilder;
+import com.azure.android.core.test.HttpClientTestsWireMockServer;
+import com.azure.android.core.test.http.HttpClientTests;
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class HttpUrlConnectionHttpClientTests extends HttpClientTests {
+    private static WireMockServer server;
+
+    @BeforeAll
+    public static void getWireMockServer() {
+        server = HttpClientTestsWireMockServer.getHttpClientTestsServer();
+        server.start();
+    }
+
+    @AfterAll
+    public static void shutdownWireMockServer() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    @Override
+    protected int getWireMockPort() {
+        return server.port();
+    }
+
+    @Override
+    protected HttpClient createHttpClient() {
+        return new HttpUrlConnectionAsyncHttpClientBuilder().build();
+    }
+}
