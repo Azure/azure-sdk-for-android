59607c89bc966227e9aac644d3c9695285a8a07a
Minnie Liu <peiliu@microsoft.com>
2021-03-09 09:51:36 -0800

    Address comments

--- sdk/communication/azure-communication-common/src/main/java/com/azure/android/communication/common/CommunicationCloudEnvironment.java
+++ sdk/communication/azure-communication-common/src/main/java/com/azure/android/communication/common/CommunicationCloudEnvironment.java
@@ -12,13 +12,13 @@ public final class CommunicationCloudEnvironment {
     private static final String DOD_VALUE = "dod";
     private static final String GCCH_VALUE = "gcch";
 
-    private String environmentValue;
+    private static String environmentValue;
 
     /**
-     * Creates a default CommunicationCloudEnvironment
+     * Creates a CommunicationCloudEnvironment
      */
-    public CommunicationCloudEnvironment() {
-        this.environmentValue = PUBLIC_VALUE;
+    public CommunicationCloudEnvironment(String environmentValue) {
+        this.environmentValue = environmentValue;
     }
 
     /**
@@ -26,26 +26,25 @@ public final class CommunicationCloudEnvironment {
      * @param environmentValue string representation of the environment value
      * @return CommunicationCloudEnvironment object itself
      */
-    public final CommunicationCloudEnvironment fromString(String environmentValue) {
+    public final static CommunicationCloudEnvironment fromString(String environmentValue) {
         Objects.requireNonNull(environmentValue);
-        this.environmentValue = environmentValue;
-        return this;
+        return new CommunicationCloudEnvironment(environmentValue);
     }
 
     /**
      * Represent Azure public cloud
      */
-    public static final CommunicationCloudEnvironment PUBLIC = new CommunicationCloudEnvironment().fromString(PUBLIC_VALUE);
+    public static final CommunicationCloudEnvironment PUBLIC = CommunicationCloudEnvironment.fromString(PUBLIC_VALUE);
 
     /**
      * Represent Azure Dod cloud
      */
-    public static final CommunicationCloudEnvironment DOD = new CommunicationCloudEnvironment().fromString(DOD_VALUE);
+    public static final CommunicationCloudEnvironment DOD = CommunicationCloudEnvironment.fromString(DOD_VALUE);
 
     /**
      * Represent Azure Gcch cloud
      */
-    public static final CommunicationCloudEnvironment GCCH = new CommunicationCloudEnvironment().fromString(GCCH_VALUE);
+    public static final CommunicationCloudEnvironment GCCH = CommunicationCloudEnvironment.fromString(GCCH_VALUE);
 
     @Override
     public boolean equals(Object that) {
