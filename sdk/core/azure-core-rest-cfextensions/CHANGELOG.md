65cf40de511fe08c01e84368094ee796589bcfd7
Anu Thomas Chandy <anuamd@hotmail.com>
2021-05-20 23:34:45 -0700

    Removing empty (unused) azure-core-rest-cfextensions module

--- sdk/core/azure-core-rest-cfextensions/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Release History
-
-## 1.0.0-beta.6 (Unreleased)
-
-## 1.0.0-beta.4 (2021-03-18)
-
-- Initial release. Please see the README for information.
--- sdk/core/azure-core-rest-cfextensions/src/main/java/com/azure/android/core/rest/cfextensions/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package containing CompletableFuture extensions for azure-core-rest module.
- */
-package com.azure.android.core.rest.cfextensions;
