e3cec41df771fbe138bfe0c69fd5cf491a69be06
Chidozie Ononiwu (His Righteousness) <31145988+chidozieononiwu@users.noreply.github.com>
2021-08-31 14:40:37 -0700

    Add Get-AllPackageInfoFromRepo function (#846)
    * Add Get-AllPackageInfoFromRepo function
    
    * Add SetPackageVersion function

---
+++ eng/scripts/Language-Settings.ps1
@@ -0,0 +1,43 @@
+$Language = "android"
+
+function Get-AllPackageInfoFromRepo ($serviceDirectory)
+{
+  $allPackageProps = @()
+  Push-Location $RepoRoot
+
+  if ($serviceDirectory)
+  {
+    $properties = gradle ":sdk:$serviceDirectory:retrieveProjectProperties" -q
+  }
+  else
+  {
+    $properties = gradle retrieveProjectProperties -q
+  }
+  Pop-Location
+  foreach($line in $properties)
+  {
+    if ($line -match "^(?<version>.*)?~(?<name>.*)?~(?<group>.*)~(?<projDir>.*)")
+    {
+      $pkgName = $Matches["name"]
+      $pkgVersion = $Matches["version"]
+      $pkgGroup = $Matches["group"]
+      $pkgPath = $Matches["projDir"]
+      $serviceDirectory = Split-Path -Path $pkgPath -Parent
+      $serviceDirName = Split-Path -Path $serviceDirectory -Leaf
+
+      $pkgProp = [PackageProps]::new($pkgName, $pkgVersion, $pkgPath, $serviceDirName, $pkgGroup)
+      $allPackageProps += $pkgProp
+    }
+    
+  }
+  return $allPackageProps
+}
+
+function SetPackageVersion ($PackageName, $Version, $ReleaseDate)
+{
+  if($null -eq $ReleaseDate)
+  {
+    $ReleaseDate = Get-Date -Format "yyyy-MM-dd"
+  }
+  & "$EngDir/scripts/Update-PkgVersion.ps1" -PackageName $PackageName -NewVersionString $Version -ReleaseDate $ReleaseDate
+}
