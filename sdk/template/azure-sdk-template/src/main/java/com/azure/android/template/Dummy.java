323e60eb0eb1b370f9d633a6d6c1e8ae99495509
Victor Colin Amador <vicolina@microsoft.com>
2022-03-15 15:44:23 -0700

    Added a Template module to test CI.

---
+++ sdk/template/azure-sdk-template/src/main/java/com/azure/android/template/Dummy.java
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.template;
+
+/**
+ * Dummy placeholder class
+ */
+public final class Dummy {
+    /**
+     * Creates a new {@link Dummy} object.
+     */
+    public Dummy() {
+    }
+
+    /**
+     * Does nothing.
+     */
+    public static void doNothing() {
+    }
+}
