5fd162363be5f78b58f2050674ea91dc3bf768fd
vcolin7 <vicolina@microsoft.com>
2022-03-16 11:05:17 -0700

    Added a Template module to test CI. (#1054)

---
+++ sdk/template/azure-sdk-template/src/main/java/com/azure/android/template/package-info.java
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/** Package containing the classes for AzureTemplate.*/
+package com.azure.android.template;
