4e11b43b3542ef6577c8f8e6b0a11199c6275f2d
jiminwen <jiminwen@microsoft.com>
2022-05-02 13:31:06 -0700

    Modifying

--- samples/sample-chat-app/src/main/java/com/azure/android/communication/chat/sampleapp/ApplicationConstants.java
+++ samples/sample-chat-app/src/main/java/com/azure/android/communication/chat/sampleapp/ApplicationConstants.java
@@ -2,11 +2,11 @@ package com.azure.android.communication.chat.sampleapp;
 
 public class ApplicationConstants {
     // Replace firstUserId and secondUserId with valid communication user identifiers from your ACS instance.
-    public final static String firstUserId = "8:acs:357e39d2-a29a-4bf6-88cc-fda0afc2c0ed_00000011-17a9-b6a3-9ffb-9c3a0d00f6ea";
+    public final static String firstUserId = "";
     public final static String secondUserId = "";
     // Replace userAccessToken with a valid communication service token for your ACS instance.
-    public static final String firstUserAccessToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjEwNCIsIng1dCI6IlJDM0NPdTV6UENIWlVKaVBlclM0SUl4Szh3ZyIsInR5cCI6IkpXVCJ9.eyJza3lwZWlkIjoiYWNzOjM1N2UzOWQyLWEyOWEtNGJmNi04OGNjLWZkYTBhZmMyYzBlZF8wMDAwMDAxMS0xN2E5LWI2YTMtOWZmYi05YzNhMGQwMGY2ZWEiLCJzY3AiOjE3OTIsImNzaSI6IjE2NTE1MTE3MzAiLCJleHAiOjE2NTE1OTgxMzAsImFjc1Njb3BlIjoiY2hhdCIsInJlc291cmNlSWQiOiIzNTdlMzlkMi1hMjlhLTRiZjYtODhjYy1mZGEwYWZjMmMwZWQiLCJpYXQiOjE2NTE1MTE3MzB9.AFelkR6oHggJSKpm6zwTHO3cu9jiQzTO7nxrjsW9qRQ7rgdJOsNpaXvUsESTE1uqGxSPbxYpOQJhfItdJCdtf7Dj51rYpK6ZuBSAGYALKmwz2INUnSGgot1D2n4G82FIiT2oBWzQsb12fxSqvOJqrRG9aQrh9VZ0D5mBOa1KFKM780cwzANU0geW88xD37bYj2LCQB5EcdNZ3vprcbrE5bCgEFBwNDBuptWJ2m7T_NFxlWm-qy6gW-AZc3wxjX208BdsmXfvJK7tBX3MunZPzrDPb9Q7wxy166nLGj6oMBXnrED1qhcFWSaCXIQOQcDY27DfQHAOKKahLkef8kBFpg";
-    public static final String endpoint = "https://chat-sdktester-e2e.communication.azure.com";
+    public static final String firstUserAccessToken = "";
+    public static final String endpoint = "";
     public static final String sdkVersion = "1.2.0-beta.1";
     public final static String SDK_NAME = "azure-communication-com.azure.android.communication.chat";
     public final static String APPLICATION_ID = "Chat_Test_App";
