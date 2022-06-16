014e8d4a6f361e73dabd6621ca2f36d5b4d2ee0a
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2022-06-16 08:40:29 -0700

    removing override hashCode

--- sdk/communication/azure-communication-common/src/main/java/com/azure/android/communication/common/CommunicationUserIdentifier.java
+++ sdk/communication/azure-communication-common/src/main/java/com/azure/android/communication/common/CommunicationUserIdentifier.java
@@ -43,9 +43,4 @@ public final class CommunicationUserIdentifier extends CommunicationIdentifier {
 
         return ((CommunicationUserIdentifier) that).getRawId().equals(getRawId());
     }
-
-    @Override
-    public int hashCode() {
-        return getRawId().hashCode();
-    }
 }
