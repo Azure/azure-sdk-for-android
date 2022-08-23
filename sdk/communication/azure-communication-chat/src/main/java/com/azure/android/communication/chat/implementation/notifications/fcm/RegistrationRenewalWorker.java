35672e2ee6d0c05ab03d16da45172a1de49b6a4b
jiminwen-msft <97924128+jiminwen-msft@users.noreply.github.com>
2022-08-23 16:01:42 -0700

    Update sdk/communication/azure-communication-chat/src/main/java/com/azure/android/communication/chat/implementation/notifications/fcm/RegistrationRenewalWorker.java
    Co-authored-by: vcolin7 <vicolina@microsoft.com>

--- sdk/communication/azure-communication-chat/src/main/java/com/azure/android/communication/chat/implementation/notifications/fcm/RegistrationRenewalWorker.java
+++ sdk/communication/azure-communication-chat/src/main/java/com/azure/android/communication/chat/implementation/notifications/fcm/RegistrationRenewalWorker.java
@@ -37,7 +37,7 @@ public class RegistrationRenewalWorker extends Worker {
     public RegistrationRenewalWorker(@NonNull Context context, @NonNull WorkerParameters workerParameters) {
         super(context, workerParameters);
         throw clientLogger.logExceptionAsError(new RuntimeException("Missing WorkManager configuration for enabling push notifications. "
-            + "Please follow push notification guidance to add missed steps: "
+            + "Please refer to the following documentation to ensure the correct setup is applied: "
             + "https://docs.microsoft.com/azure/communication-services/tutorials/chat-android-push-notification"));
 
     }
