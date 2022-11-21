a9d9592c8c1a6c0c9c64bdf615c0bc352e754cbb
jiminwen-msft <jiminwen@microsoft.com>
2022-11-21 14:57:54 -0800

    fix version

--- samples/sample-chat-app/src/main/java/com/azure/android/communication/chat/sampleapp/ApplicationConstants.java
+++ samples/sample-chat-app/src/main/java/com/azure/android/communication/chat/sampleapp/ApplicationConstants.java
@@ -4,7 +4,7 @@
 package com.azure.android.communication.chat.sampleapp;
 
 public class ApplicationConstants {
-    public final static String SDK_VERSION = "1.2.0-beta.1";
+    public final static String SDK_VERSION = "2.1.0";
     public final static String SDK_NAME = "azure-communication-com.azure.android.communication.chat";
     public final static String APPLICATION_ID = "Chat_Test_App";
     public final static String TAG = "[Chat Test App]";
