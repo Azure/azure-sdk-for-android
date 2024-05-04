ecbb3598eb2c21f23338f05ebe0a89f9fba2710e
Bas Hendri <dipidoo@users.noreply.github.com>
2024-05-04 02:07:54 +0000

    ACS Calling docs update

---
+++ azure-core-http-okhttp/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.azure.android.core.http.okhttp"}]
---
+++ azure-core/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.azure.android.core.exception"},{"l":"com.azure.android.core.util"},{"l":"com.azure.android.core.util.paging"}]
